52d1af6ac7a82e9efb29cec07af2942be9de32c2
carlodoneddu <donedduc@gmail.com>
2026-06-23 23:57:40 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { client, bioQuery, progettiAttiviQuery, progettiPassatiQuery, collaborazioniQuery, nuovoDiscoQuery, concertiQuery } from '../lib/sanity'
+import { client, bioQuery, progettiAttiviQuery, progettiPassatiQuery, collaborazioniQuery, nuovoDiscoQuery, concertiQuery, urlFor } from '../lib/sanity'
 import Nav from '../components/Nav'
 import Hero from '../components/Hero'
 import Bio from '../components/Bio'
@@ -10,30 +10,37 @@ import Footer from '../components/Footer'
 
 export const revalidate = 60
 
+const siteSettingsQuery = `*[_type == "siteSettings"][0]{ heroImmagine }`
+
 async function getData() {
   try {
-    const [bio, progettiAttivi, progettiPassati, collaborazioni, nuovoDisco, concerti] = await Promise.all([
+    const [bio, progettiAttivi, progettiPassati, collaborazioni, nuovoDisco, concerti, siteSettings] = await Promise.all([
       client.fetch(bioQuery),
       client.fetch(progettiAttiviQuery),
       client.fetch(progettiPassatiQuery),
       client.fetch(collaborazioniQuery),
       client.fetch(nuovoDiscoQuery),
       client.fetch(concertiQuery),
+      client.fetch(siteSettingsQuery),
     ])
-    return { bio, progettiAttivi, progettiPassati, collaborazioni, nuovoDisco, concerti }
+    return { bio, progettiAttivi, progettiPassati, collaborazioni, nuovoDisco, concerti, siteSettings }
   } catch {
-    return { bio: null, progettiAttivi: [], progettiPassati: [], collaborazioni: [], nuovoDisco: null, concerti: [] }
+    return { bio: null, progettiAttivi: [], progettiPassati: [], collaborazioni: [], nuovoDisco: null, concerti: [], siteSettings: null }
   }
 }
 
 export default async function Home() {
-  const { bio, progettiAttivi, progettiPassati, collaborazioni, nuovoDisco, concerti } = await getData()
+  const { bio, progettiAttivi, progettiPassati, collaborazioni, nuovoDisco, concerti, siteSettings } = await getData()
+
+  const heroImageUrl = siteSettings?.heroImmagine
+    ? urlFor(siteSettings.heroImmagine).width(1920).quality(85).url()
+    : null
 
   return (
     <>
       <Nav />
       <main>
-        <Hero />
+        <Hero heroImage={heroImageUrl} />
         <Bio data={bio} />
         <NuovoDisco data={nuovoDisco} />
         <Progetti attivi={progettiAttivi} passati={progettiPassati} />
